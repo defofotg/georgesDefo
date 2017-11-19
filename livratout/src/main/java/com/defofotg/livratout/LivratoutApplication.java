8e058345f20d5023e1a8a13f171622be500971aa
defofotg <defo.georges@gmail.com>
2017-11-19 15:44:56 +0100

    first commit

---
+++ livratout/src/main/java/com/defofotg/livratout/LivratoutApplication.java
@@ -0,0 +1,12 @@
+package com.defofotg.livratout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivratoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivratoutApplication.class, args);
+	}
+}
---
+++ livratout/src/main/java/com/defofotg/livratout/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.defofotg.livratout;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LivratoutApplication.class);
+	}
+
+}
